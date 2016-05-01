0c8c706d4576e509fe0c04585c627af6c19f66bc
nodesmet <noe.desmet@gmail.com>
2016-05-02 00:41:12 +0200

    meetActivity, dispo, rdv (layout+java)

---
+++ app/src/main/java/com/epl/uclouvain/uclove/Disponibility.java
@@ -0,0 +1,23 @@
+package com.epl.uclouvain.uclove;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+/**
+ * Created by noe on 02/05/16.
+ */
+public class Disponibility extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.disponibility);
+
+    }
+}
