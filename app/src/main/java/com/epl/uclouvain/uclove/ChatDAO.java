64dac8c62d3673966403575f1dcee38850cf32fd
delphon <delphine.detrem@gmail.com>
2016-05-02 22:40:01 +0200

    chat création !! non terminer

--- app/src/main/java/com/epl/uclouvain/uclove/ChatDAO.java
+++ app/src/main/java/com/epl/uclouvain/uclove/ChatDAO.java
@@ -27,6 +27,7 @@ public class ChatDAO extends DAOBase
     }
 
 
+
     public ArrayList<ChatMessage> historique(String l1, String l2)
     {
         Cursor c = mDb.rawQuery("select login1, login2, msg, msg_date" +
