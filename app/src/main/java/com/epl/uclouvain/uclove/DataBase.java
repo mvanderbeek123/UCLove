e29541f1fda3d3725590f126acce9cb1c3ecff94
nodesmet <noe.desmet@gmail.com>
2016-05-04 00:24:36 +0200

    Merge branch 'master' of /home/noe/UCLove with conflicts.

--- app/src/main/java/com/epl/uclouvain/uclove/DataBase.java
+++ app/src/main/java/com/epl/uclouvain/uclove/DataBase.java
@@ -5,11 +5,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
- * La base de données.
+ * La base de données. Que chacun ajoute les tables qu'il lui faut.
  */
 public class DataBase extends SQLiteOpenHelper
 {
-    // Table Client
+// Table Client
     public static final String CLIENT_TABLE_NAME="Client";
     public static final String CLIENT_LOGIN="login";
     public static final String CLIENT_MDP="mot_de_passe";
@@ -25,6 +25,7 @@ public class DataBase extends SQLiteOpenHelper
     public static final String CLIENT_SKIN="peau";
     public static final String CLIENT_AGE="tranche_age";
     public static final String CLIENT_LOC="perimetre";
+
     public static final String CLIENT_CREATE_TABLE = "CREATE TABLE " + CLIENT_TABLE_NAME + " (" +
             CLIENT_LOGIN + " TEXT NOT NULL PRIMARY KEY, " +
             CLIENT_MDP  + " TEXT NOT NULL, " +
@@ -42,20 +43,22 @@ public class DataBase extends SQLiteOpenHelper
             CLIENT_LOC + " INTEGER);";
     public static final String CLIENT_DROP =
             "DROP TABLE IF EXISTS " + CLIENT_TABLE_NAME + ";";
-
     // Table Genre
     public static final String GENRE_TABLE_NAME="Genre";
     public static final String GENRE_LOGIN="login";
     public static final String GENRE_GENRE="genre";
+
     public static final String GENRE_CREATE_TABLE= "CREATE TABLE " + GENRE_TABLE_NAME + " (" +
             GENRE_LOGIN + " TEXT NOT NULL REFERENCES CLIENT, " +
             GENRE_GENRE + " TEXT NOT NULL);";
+
     public static final String GENRE_DROP = "DROP TABLE IF EXISTS " + GENRE_TABLE_NAME+";";
 
     // Table Cheveuxc
     public static final String CHEVEUX_TABLE_NAME="Cheveux";
     public static final String CHEVEUX_LOGIN="login";
     public static final String CHEVEUX_COLOR="couleur";
+
     public static final String CHEVEUX_CREATE_TABLE= "CREATE TABLE " + CHEVEUX_TABLE_NAME + " (" +
             CHEVEUX_LOGIN + " TEXT NOT NULL REFERENCES CLIENT, " +
             CHEVEUX_COLOR + " TEXT NOT NULL);";
@@ -65,38 +68,40 @@ public class DataBase extends SQLiteOpenHelper
     public static final String YEUX_TABLE_NAME="Yeux";
     public static final String YEUX_LOGIN="login";
     public static final String YEUX_COLOR="couleur";
+
     public static final String YEUX_CREATE_TABLE= "CREATE TABLE " + YEUX_TABLE_NAME + "(" +
             YEUX_LOGIN + " TEXT NOT NULL REFERENCES CLIENT, " +
             YEUX_COLOR + " TEXT NOT NULL);";
     public static final String YEUX_DROP = "DROP TABLE IF EXISTS " + YEUX_TABLE_NAME+";";
-
     // Table Peau
     public static final String PEAU_TABLE_NAME="Peau";
     public static final String PEAU_LOGIN="login";
     public static final String PEAU_COLOR="couleur";
+
     public static final String PEAU_CREATE_TABLE= "CREATE TABLE " + PEAU_TABLE_NAME + "(" +
             PEAU_LOGIN + " TEXT NOT NULL REFERENCES CLIENT, " +
             PEAU_COLOR + " TEXT NOT NULL);";
     public static final String PEAU_DROP = "DROP TABLE IF EXISTS " + PEAU_TABLE_NAME+";";
-
     // Table Etude
     public static final String ETUDE_TABLE_NAME="Etude";
     public static final String ETUDE_LOGIN="login";
     public static final String ETUDE="faculte";
+
     public static final String ETUDE_CREATE_TABLE= "CREATE TABLE " + ETUDE_TABLE_NAME + " (" +
             ETUDE_LOGIN + " TEXT NOT NULL REFERENCES CLIENT, " +
             ETUDE + " TEXT NOT NULL);";
     public static final String ETUDE_DROP = "DROP TABLE IF EXISTS " + ETUDE_TABLE_NAME+";";
-
     // Table amis.
     public static final String AMIS_TABLE_NAME = "Amis";
     public static final String AMIS_KEY = "id";
     public static final String AMIS_LOGIN = "login";
     public static final String AMIS_ISAMI = "isAmi";
+
     public static final String AMIS_TABLE_CREATE = "CREATE TABLE " + AMIS_TABLE_NAME + " (" +
             AMIS_KEY + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             AMIS_LOGIN + " TEXT, " +
             AMIS_ISAMI + " INTEGER " + ");";
+
     public static final String AMIS_TABLE_DROP = "DROP TABLE IF EXISTS " + AMIS_TABLE_NAME + ";";
 
     // Table chatmessages
@@ -105,12 +110,26 @@ public class DataBase extends SQLiteOpenHelper
     public static final String LOGIN2 = "login2";
     public static final String MSG = "msg";
     public static final String MSGDATE = "msg_date";
-    public static final String CHAT_CREATE_TABLE = "CREATE TABLE " + CHAT_TABLE_NAME + " (" +
+
+    public static final String CHAT_TABLE_CREATE = "CREATE TABLE " + CHAT_TABLE_NAME + " (" +
             LOGIN1 + " TEXT, " +
             LOGIN2 + " TEXT, " +
             MSG + " TEXT " +
             MSGDATE + " TEXT " +");";
+
     public static final String CHAT_TABLE_DROP = "DROP TABLE IF EXISTS " + CHAT_TABLE_NAME + ";";
+
+    public static final String MEET_TABLE_NAME = "Meet";
+    public static final String MEET_LOGIN = "login";
+    public static final String MEET_DATE = "Date";
+    public static final String MEET_HOUR = "Heure";
+
+    public static final String MEET_TABLE_CREATE = "CREATE TABLE " + MEET_TABLE_NAME + " (" +
+            MEET_LOGIN + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            MEET_DATE + " TEXT, " +
+            MEET_HOUR + " INTEGER " + ");";
+
+    public static final String MEET_TABLE_DROP = "DROP TABLE IF EXISTS " + MEET_TABLE_NAME + ";";
 
 
     public DataBase(Context context, String name, SQLiteDatabase.CursorFactory factory, int version)
@@ -123,7 +142,8 @@ public class DataBase extends SQLiteOpenHelper
     {
         db.execSQL(AMIS_TABLE_CREATE);
         db.execSQL(CLIENT_CREATE_TABLE);
-        db.execSQL(CHAT_CREATE_TABLE);
+        db.execSQL(CHAT_TABLE_CREATE);
+        db.execSQL(MEET_TABLE_CREATE);
         db.execSQL(GENRE_CREATE_TABLE);
         db.execSQL(CHEVEUX_CREATE_TABLE);
         db.execSQL(YEUX_CREATE_TABLE);
@@ -137,6 +157,7 @@ public class DataBase extends SQLiteOpenHelper
         db.execSQL(AMIS_TABLE_DROP);
         db.execSQL(CLIENT_DROP);
         db.execSQL(CHAT_TABLE_DROP);
+        db.execSQL(MEET_TABLE_DROP);
         db.execSQL(GENRE_DROP);
         db.execSQL(CHEVEUX_DROP);
         db.execSQL(YEUX_DROP);
