2002b4b5361dc470c0618c9c2823fa63759df747
mvanderbeek123 <mvanderbeek123@users.noreply.github.com>
2016-05-04 08:53:18 +0200

    ajout de la classe InscriptionActivity

--- app/src/main/java/com/epl/uclouvain/uclove/PageAcceuil.java
+++ app/src/main/java/com/epl/uclouvain/uclove/PageAcceuil.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by marie-marie on 21/04/16.
@@ -16,13 +18,15 @@ public class PageAcceuil extends Activity implements View.OnTouchListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pageacceuil);
         Button b = (Button) findViewById(R.id.connexion);
-        // Puis on lui indique que cette classe sera son listener pour l'évènement Touch
+        EditText login=(EditText) findViewById(R.id.Login);
+        EditText mdp= (EditText) findViewById(R.id.motdepasse);
         b.setOnTouchListener(this);
     }
 
     // Fonction qui sera lancée à chaque fois qu'un toucher est détecté sur le bouton rattaché
     @Override
     public boolean onTouch(View view, MotionEvent event) {
+
         // Comme l'évènement nous donne la vue concernée par le toucher, on le récupère et on le caste en Button
         Intent intent = new Intent(PageAcceuil.this, Menu.class);
         startActivity(intent);
