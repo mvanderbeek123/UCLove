20ac2c8dddfb3114a8150ebcf959bea062b1ea41
nodesmet <noe.desmet@gmail.com>
2016-05-05 12:28:29 +0200

    meet plus de bugs

--- app/src/main/java/com/epl/uclouvain/uclove/DisponibilityActivity.java
+++ app/src/main/java/com/epl/uclouvain/uclove/DisponibilityActivity.java
@@ -8,16 +8,28 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.CalendarView;
+
+import java.util.Date;
 
 /**
  * Created by noe on 02/05/16.
  */
-public class Disponibility extends Activity {
+public class DisponibilityActivity extends Activity {
+
+    CalendarView calendar;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.disponibility);
+
+        calendar=(CalendarView) findViewById(R.id.calendarView);
+        Date date=new Date(calendar.getDate());
+
+        //Meet m=new Meet(Controler.logged_user, date.getDate(), date.getHours(), date.getHours()+3);
+        //MeetDAO.ajouter(m);
+
 
     }
 }
--- app/src/main/java/com/epl/uclouvain/uclove/MeetDAO.java
+++ app/src/main/java/com/epl/uclouvain/uclove/MeetDAO.java
@@ -7,12 +7,13 @@ import android.content.Context;
  * Classe DAO pour la table MEET de la database
  * Created by noe on 04/05/16.
  */
-//public class MeetDAO extends DAOBase {
+    public class MeetDAO extends DAOBase {
 
-    /*public static final String TABLE_NAME = "Meet";
+    public static final String TABLE_NAME = "Meet";
     public static final String LOGIN = "login"; //voir si on travaille par login ou par id
     public static final String DATE = "date";
-    public static final String HOUR = "hour";
+    public static final String HOURSTART = "hourStart";
+    public static final String HOUREND = "hourEnd";
 
     public MeetDAO(Context pContext)
     {
@@ -24,10 +25,11 @@ import android.content.Context;
         ContentValues value = new ContentValues();
         value.put(MeetDAO.LOGIN, m.getLogin());
         value.put(MeetDAO.DATE, m.getDate());
-        value.put(MeetDAO.HOUR, m.getHour());
+        value.put(MeetDAO.HOURSTART, m.getHourStart());
+        value.put(MeetDAO.HOUREND, m.getHourEnd());
         mDb.insert(MeetDAO.TABLE_NAME, null, value);
     }
 
     //public void ajouter_dispo{} */
 
-//}
+    }
