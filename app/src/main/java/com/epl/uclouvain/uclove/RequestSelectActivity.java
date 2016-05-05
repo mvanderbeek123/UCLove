83867f571b43827d3f0a5fe79bd2494a0783167b
smarneffe <stephan.marneffe@student.uclouvain.be>
2016-05-05 12:20:58 +0200

    Request 1.0

--- app/src/main/java/com/epl/uclouvain/uclove/RequestSelectActivity.java
+++ app/src/main/java/com/epl/uclouvain/uclove/RequestSelectActivity.java
@@ -1,6 +1,8 @@
 package com.epl.uclouvain.uclove;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -25,7 +27,7 @@ public class RequestSelectActivity extends Activity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.request_select);
-
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         listView = (ListView) findViewById(R.id.listAmis);
         logins = new ArrayList<String>();
 
@@ -34,29 +36,46 @@ public class RequestSelectActivity extends Activity
         ArrayList<Amis> alist = adao.selectionner_listRequetes(Controler.logged_user);
         adao.close();
 
+        if(alist.size()==0)
+        {
+            setContentView(R.layout.blank);
+            alertDialogBuilder.setTitle("Request Message");
+            alertDialogBuilder.setMessage("You don't have new request");
+            alertDialogBuilder.setCancelable(false);
+            alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    //Rajouter la requete dans la base de donnée
+                    RequestSelectActivity.this.finish();
+                }
+            });
+            AlertDialog theAlert = alertDialogBuilder.create();
+            theAlert.show();
 
-        for (int i = 0; i < alist.size(); i++) {
-            Amis amis = alist.get(i);
-            String log1 = amis.getLogin1();
-            String log2 = amis.getLogin2();
-            if (log1.equals(Controler.logged_user))
-                logins.add(log2);
-            else
-                logins.add(log1);
         }
-
-        amiAdapteur = new ArrayAdapter<String>(RequestSelectActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, logins);
-        listView.setAdapter(amiAdapteur);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Controler.chat_user = logins.get(position);
-                // id n'est pas l'id de l'ami mais celui de la vue, il faut encore le récupérer.
-                Intent i = new Intent(RequestSelectActivity.this, MyRequests.class);
-                startActivity(i);
+        else {
+            for (int i = 0; i < alist.size(); i++) {
+                Amis amis = alist.get(i);
+                String log1 = amis.getLogin1();
+                String log2 = amis.getLogin2();
+                if (log1.equals(Controler.logged_user))
+                    logins.add(log2);
+                else
+                    logins.add(log1);
             }
-        });
+
+            amiAdapteur = new ArrayAdapter<String>(RequestSelectActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, logins);
+            listView.setAdapter(amiAdapteur);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Controler.chat_user = logins.get(position);
+                    // id n'est pas l'id de l'ami mais celui de la vue, il faut encore le récupérer.
+                    Intent i = new Intent(RequestSelectActivity.this, MyRequests.class);
+                    startActivity(i);
+                }
+            });
+        }
 
     }
 }
