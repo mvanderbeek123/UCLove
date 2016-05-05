95630bb1327a37d99292aadbe752a4598bb313c7
mvanderbeek123 <mvanderbeek123@users.noreply.github.com>
2016-05-05 11:30:55 +0200

    inscription layout corrigé

--- app/src/main/java/com/epl/uclouvain/uclove/InscriptionActivity.java
+++ app/src/main/java/com/epl/uclouvain/uclove/InscriptionActivity.java
@@ -76,6 +76,7 @@ public class InscriptionActivity extends Activity {
                 profildao.open();
                 profildao.ajouter(profil);
                 profildao.close();
+
                 //Intent intent = new Intent(InscriptionActivity.this, ProfilActivity.class);
                 //startActivity(intent);
                 Toast toast=Toast.makeText(getApplicationContext(),"Vous n'êtes actuellement pas encore inscrit. Enregistrez-vous et venez nous rejoindre!",Toast.LENGTH_LONG);
